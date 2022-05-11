d015191af99101f5cb0928d03a74fb10b5088f42
Hyunwoo Cho <barefoot1423@gmail.com>
2022-05-10 22:46:32 -0400

    Initial commit

---
+++ src/main/java/v1/ch2/Welcome.java
@@ -0,0 +1,14 @@
+package v1.ch2;
+
+public class Welcome {
+
+    public static void main(String[] args) {
+        String greeting = "Welcome to Core Java!";
+        System.out.println(greeting);
+        for (int i = 0; i < greeting.length(); i++) {
+            System.out.print("=");
+        }
+        System.out.println();
+    }
+
+}
