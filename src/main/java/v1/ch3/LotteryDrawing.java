3fe3fdcc23aeac6d487f85e323cc033e907feed8
Hyunwoo Cho <barefoot1423@gmail.com>
2022-12-07 18:10:38 -0500

    lotto exampleˆ

---
+++ src/main/java/v1/ch3/LotteryDrawing.java
@@ -0,0 +1,37 @@
+package v1.ch3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LotteryDrawing {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("How many numbers do you need to draw? ");
+        int k = in.nextInt();
+
+        System.out.println("What is the hightest number you can draw? ");
+        int n = in.nextInt();
+
+        int[] numbers = new int[n];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 1;
+        }
+
+        int[] result = new int[k];
+        for (int i = 0; i < result.length; i++) {
+            int r = (int) (Math.random() * n);
+
+            result[i] = numbers[r];
+
+            numbers[r] = numbers[n - 1];
+            n--;
+        }
+
+        Arrays.sort(result);
+        System.out.println("Bet the following combination. It'll make you rich!");
+        System.out.println(Arrays.toString(result));
+    }
+
+}
