fe2293bd637879f6c77bed2e4db53de04841382f
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-28 00:23:27 -0500

    commit

--- src/main/java/v1/ch4/EmployeeTest.java
+++ src/main/java/v1/ch4/EmployeeTest.java
@@ -10,7 +10,7 @@ public class EmployeeTest {
         staff[2] = new Employee("Tony Tester", 40000, 1990, 3, 15);
 
         for (Employee e : staff) {
-            e.raiseSalary(6);
+            e.raiseSalary(7);
         }
 
         for (Employee e : staff) {
